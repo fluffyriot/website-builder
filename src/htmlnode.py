aaa2c317d3325154b239e951c29e5032b98f82bc
fluffyriot <me@fluffyriot.com>
2025-12-18 23:20:55 +0000

    ch2l6

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,18 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag to convert to HTML")
+        if self.children is None:
+            raise ValueError("ParentNode must have children to convert to HTML")
+        children_html = "".join([child.to_html() for child in self.children])
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -11,7 +12,7 @@ class TextType(Enum):
 class TextNode:
     def __init__(self, text, text_type, url = None):
         self.text = text
-        self.text_type = TextType(text_type)
+        self.text_type = text_type
         self.url = url
     
     def __eq__(self, other):
@@ -22,3 +23,19 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    else:
+        raise ValueError(f"Unsupported text type: {text_node.text_type}")
