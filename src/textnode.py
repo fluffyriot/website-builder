9fa9c524967052f1697e084a77fef1875b7d602c
fluffyriot <me@fluffyriot.com>
2025-12-18 22:36:00 +0000

    sync ch2l3

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -15,7 +15,10 @@ class TextNode:
         self.url = url
     
     def __eq__(self, other):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (self.text == other.text
+                and self.text_type == other.text_type
+                and self.url == other.url
+        )
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
