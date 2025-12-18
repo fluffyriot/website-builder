51b20c7e670a8279737be2fc0f5097612101b95b
fluffyriot <me@fluffyriot.com>
2025-12-18 23:47:31 +0000

    ch3l1

---
+++ src/node_split.py
@@ -0,0 +1,32 @@
+from textnode import TextNode, TextType
+
+delimiters = {
+    "`": TextType.CODE,
+    "_": TextType.ITALIC,
+    "**": TextType.BOLD
+}
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    return_list = []
+
+    if delimiter not in delimiters:
+        raise ValueError(f"Delimiter {delimiter} not supported.")
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            return_list.append(node)
+            continue
+        split_nodes = []
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(parts)):
+            if parts[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(parts[i], delimiters[delimiter]))
+        return_list.extend(split_nodes)
+    
+    return return_list
