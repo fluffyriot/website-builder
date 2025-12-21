f4715fdff937e5a4ac7ea1a4e03a4976a3e66454
fluffyriot <me@fluffyriot.com>
2025-12-21 18:16:39 +0000

    sync to the last stage

---
+++ src/gencontent.py
@@ -0,0 +1,50 @@
+import os
+from markdown_blocks import markdown_to_html_node
+import os
+import shutil
+
+
+def gen_pages_recursive(source_dir_path, dest_dir_path, template_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path) and filename == "index.md":
+            dest_path = dest_path.replace("index.md", "index.html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            gen_pages_recursive(from_path, dest_path, template_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
